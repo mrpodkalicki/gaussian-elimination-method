fdc835abe022c360f14b7cb2ac28e7cf4da6f05b
mrpodkalicki <mr.podkalicki.adrian94@gmail.com>
2019-12-06 15:25:48 +0100

    fix error division/ change_row, max_elem_column

--- gausse.py
+++ gausse.py
@@ -2,57 +2,75 @@ import numpy as np
 from fractions import Fraction
 
 
-def max_elem_column(array_data):
+def max_elem_column(array_data,index):
+    print(index,"index")
     array_size = array_data.shape
-    list_number = [array_data[x][y]  for x in range(0,array_size[0]) for y in range(array_size[1]) if y==0]
+    list_number = [array_data[x][y]  for x in range(0,array_size[0]) for y in range(array_size[1]) if y==index and x>=index]
+    # print(list_number,'list_numb')
     max_number=max(list_number)
-    index = np.where(array_data == max_number)[0][0]
+    index = np.where(list_number == max_number)[0][0]
     index+=1
     return index, max_number
 
 
 def change_row(array_data,index_one,index_two):
+    # print(index_one,'ind_on')
+    # print(index_two,'inde_two')
     row_one = (array_data[index_one-1]).copy()
     row_two = array_data[index_two-1].copy()
     array_data[index_one-1] = row_two
     array_data[index_two-1] = row_one
+
+    # print(array_data,'change_row')
      
-
-
+def deliver_row(array_data,row_index,value):
+    row=array_data[row_index-1]
+    return np.divide(row,value)
+    
+    
 
 def reset_column_to_zero(array_data,row_index):
     array_size = array_data.shape
     value = array_data[row_index-1][row_index-1].copy()
-    print(value,'value')
     row = array_data[row_index-1].copy()
-   
-    number_list=[array_data[x][y]  for x in range(0,array_size[0]) for y in range(array_size[1]) if y == row_index-1 ]
-    print(number_list,'number_list')
+    column=[array_data[x][y]  for x in range(0,array_size[0]) for y in range(array_size[1]) if y == row_index-1 ]
     coefi_multi=[]
-    for i in number_list:
+    
+    for i in column:
         if(i!=0):
             coefi_multi.append(value/i)
         else:
             coefi_multi.append(0)
 
+    # print(coefi_multi)
     rest_array = [(array_data[x] * coefi_multi[x]) - row for x in range(0, array_size[0]) for y in range(array_size[1]) if y == row_index-1]
+
     for i in range(0,array_size[0]):
         if(i != row_index-1):
-            array_data[i] = rest_array[i]
+            if(coefi_multi[i] != 0):
+                array_data[i] = rest_array[i]
 
 
 def main():
     
-    array = np.array([[1,1,2,-1],[2,-1,2,-4],[4,1,4,2]])
+    array = np.array([[1,5,0,1],[1,2,-3,-5],[2,4,1,]])  
     array_size = array.shape
     
-    row_index = max_elem_column(array)
-    change_row(array, 1, row_index[0])
     for i in range(0, array_size[1]-1):
-       
+        # print(i,'i')
+        row_index = max_elem_column(array,i)
+        # print(row_index,'row')
+        change_row(array, i+1, row_index[0])
+        
         reset_column_to_zero(array, i+1)
-        print(array)
+        print(array,'array')
         print('next')
+    
+    value=[array[i][j] for i in range(0, array_size[0]) for j in range(0,array_size[1]) if  j==array_size[0]] 
+    variables= [array[i][j] for i in range(0, array_size[0]) for j in range(0,array_size[1]) if  j==i]
 
+
+    score = np.divide(value,variables)
+    print(score)
 
 main()
